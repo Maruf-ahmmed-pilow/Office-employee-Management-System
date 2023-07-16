a398eb4e786ce27e9e08ebceb4c704baec735311
Maruf ahmmed pilow <marufahmedpilow727@gmail.com>
2023-07-16 19:45:21 +0600

    second commit

--- office_employee_management/emp_app/admin.py
+++ office_employee_management/emp_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Depertment, Role, Employee
+
 
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(Role)
+admin.site.register(Depertment)
+
