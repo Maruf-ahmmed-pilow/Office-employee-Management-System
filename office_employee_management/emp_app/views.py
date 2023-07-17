64c088f068dd3889e0d5d9cf1f9343ad4b99f8fc
Maruf ahmmed pilow <marufahmedpilow727@gmail.com>
2023-07-17 12:16:55 +0600

    add button and html file

--- office_employee_management/emp_app/views.py
+++ office_employee_management/emp_app/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+def add_emp(request):
+    return render(request, 'add_emp.html')
+
+
+def all_emp(request):
+    return render(request, 'all_emp.html')
+
+
+def filter_emp(request):
+    return render(request, 'filter_emp.html')
+
+def remove_emp(request):
+    return render(request, 'remove_emp.html')
